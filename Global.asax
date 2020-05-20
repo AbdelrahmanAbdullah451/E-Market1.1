a8b9f04254cc73d87458c0b2e56ddd692f7ff714
Abdelrhman abduulah <abdonilsh20@fci.helwan.edu.eg>
2020-05-21 01:32:47 +0200

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="E_Market1._1.MvcApplication" Language="C#" %>
---
+++ Models/CustomizeProduct.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace E_Market1._1.Models
+{
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+        //add new property
+    }
+    public class ProductMetaData
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string name { get; set; }
+
+        [Required]
+        public Nullable<double> price { get; set; }
+
+
+        public string image { get; set; }
+
+        [Required]
+        public string description { get; set; }
+
+
+        [Required]
+        [Display (Name ="Category")]
+        public Nullable<int> category_id { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
